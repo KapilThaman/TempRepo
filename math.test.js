3d434d3e352cc299f16e57d37f3a597cea9d7666
KapilThaman <92117060+KapilThaman@users.noreply.github.com>
2022-03-01 14:54:43 -0800

    Added addition

--- math.test.js
+++ math.test.js
@@ -1,5 +1,12 @@
 const { division } = require('./division')
+const { addition } = require('./addition')
+
+
 
 test('10 / 5 = 2', () => {
 expect(division(10, 5)).toBe(2)
 })
+
+test('10 + 5 = 15', () => {
+    expect(addition(10, 5)).toBe(15)
+    })
